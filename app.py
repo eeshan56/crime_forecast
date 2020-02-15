2488c0060b5c675a3603a778931ff869d37beadf
eeshan56 <eeshanpanse@gmail.com>
2020-02-16 01:57:26 +0530

    Login and Sign Up successful with Mongo DB Atlas

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from form import RegistrationForm, LoginForm
+from pymongo import MongoClient
+from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '0f728c6b5c9f6e02690f9496da4818ae'
@@ -25,9 +27,16 @@ def forgot():
 def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		if form.email.data == 'admin@gmail.com' and form.password.data == '123456':
-			flash('You have been logged in!', 'success')
-			return redirect(url_for('home'))
+		client = MongoClient("mongodb+srv://test:test123%23@cluster0-l5ord.mongodb.net/test?retryWrites=true&w=majority")
+		db = client.get_database('user_db')
+		creds = db.user_creds
+		find_user = list(creds.find({'email':form.email.data}))
+		if len(find_user) == 1:
+			if sha256_crypt.verify(form.password.data, find_user[0]['passwd']):
+				flash('You have been logged in!', 'success')
+				return redirect(url_for('home'))
+			else:
+				flash('Please enter the correct email or password', 'danger')
 		else:
 			flash('Please enter the correct email or password', 'danger')
 
@@ -37,8 +46,22 @@ def login():
 def register():
 	form = RegistrationForm()
 	if form.validate_on_submit():
-		flash(f'Account created for {form.username.data}!', 'success')
-		return redirect(url_for('home'))
+		client = MongoClient("mongodb+srv://test:test123%23@cluster0-l5ord.mongodb.net/test?retryWrites=true&w=majority")
+		db = client.get_database('user_db')
+		creds = db.user_creds
+		dup_user = list(creds.find({'username' : form.username.data}))
+		dup_email = list(creds.find({'email' : form.email.data}))
+
+		if len(dup_user) == 0 and len(dup_email) == 0:
+			enc_pass = sha256_crypt.encrypt(form.password.data)
+			new_user = {'username' : form.username.data, 'email' : form.email.data, 'passwd' : enc_pass}
+			creds.insert_one(new_user)
+			flash(f'Account created for {form.username.data}!', 'success')
+			return redirect(url_for('home'))
+		elif len(dup_user) == 0:
+			flash(f'{form.email.data} is already in use', 'danger')
+		else:
+			flash(f'{form.username.data} is already in use', 'danger')
 	return render_template('register.html', title = 'Register', form = form)
 
 if __name__ == '__main__':
